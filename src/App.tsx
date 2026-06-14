f0ffcd17d76fda414edc7fa13025b3377c56711d
Joao <joao24perez@gmail.com>
2026-06-14 17:50:11 +0200

    feat: busqueda por nombre de pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,49 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-function App(){
-  const [pokemon, setPokemon] = useState<any[]>([]); 
+function App() {
+  const [pokemon, setPokemon] = useState<any[]>([]);
+  const [busqueda, setBusqueda] = useState("");
 
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-    .then((respuesta) => respuesta.json())
-    .then((datos) => {
-      console.log(datos.results);
-      setPokemon(datos.results);
-    })
+      .then((respuesta) => respuesta.json())
+      .then((datos) => {
+        console.log(datos.results);
+        setPokemon(datos.results);
+      })
   }, []);
 
+  const filtrados = pokemon.filter((p) =>
+    p.name.includes(busqueda.toLowerCase())
+  )
+
   return (
-    <div>
+    <div className="app">
       <h1>Pokedex</h1>
-      <div className="grid">
-        {pokemon.map((p, index) => {
-          const id = index + 1;
-          const imagen = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
-          return (
-            <div className="card" key={p.name}>
-              <img src={imagen} alt={p.name}/>
-              <p>{p.name}</p>
-            </div>
-          )
-        })}
-      </div>
+
+      <input type="text" placeholder="Buscar Pokemon" value={busqueda} onChange={(e) => setBusqueda(e.target.value)} />
+
+      {filtrados.length === 0 ? (
+        <p>No se encontro ningun Pokemon</p>
+      ) : (
+        <div className="grid">
+          {filtrados.map((p) => {
+            const id = pokemon.indexOf(p) + 1;
+            const imagen = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+            return (
+              <div className="card" key={p.name}>
+                <img src={imagen} alt={p.name} />
+                <p>{p.name}</p>
+              </div>
+            )
+          })}
+        </div>
+      )
+
+      }
+
+
     </div>
   );
 }
